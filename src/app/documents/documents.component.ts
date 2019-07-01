f7ed371d058fba4090e90c5d669a28df246ea49b
MikeWick <wickert.michael@gmail.com>
2019-07-01 14:55:41 -0400

    Create an angular interface to model data, - create mock data in an angular 2 application - give the documents component a page title

--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -1,9 +1,34 @@
 import { Component } from '@angular/core';
+import { Document } from './document';
 
 @Component({
   selector: 'documents',
   templateUrl: 'documents.component.html'
 })
 export class DocumentsComponent {
+  pageTitle: string = "Document Dashboard"
   
+  documents: Document[] = [
+    {
+      title: "My First Doc",
+      description: "asdf",
+      file_url: 'http://google.com',
+      updated_at: '01/07/19',
+      image_url: 'http://google.com'
+    },
+    {
+      title: "My Second Doc",
+      description: "asdf",
+      file_url: 'http://google.com',
+      updated_at: '01/07/19',
+      image_url: 'http://google.com'
+    },
+    {
+      title: "My Last Doc",
+      description: "asdf",
+      file_url: 'http://google.com',
+      updated_at: '01/07/19',
+      image_url: 'http://google.com'
+    },
+  ]
 }
